b90dfb9822da8562b17a77b4b864798a76095d55
Brennan Goewert <brennan@goewert.me>
2023-07-23 15:07:51 -0700

    Add Contacts dict and Contact struct

---
+++ VirtualContacts/Contact.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualContacts
+{
+    internal struct Contact
+    {
+        public string Name;
+        public int Age;
+        public string PhoneNumber;
+        public string EmailAddress;
+        public string PreferredContactMethod;
+    }
+}
--- VirtualContacts/MainForm.cs
+++ VirtualContacts/MainForm.cs
@@ -12,6 +12,9 @@ namespace VirtualContacts
 {
     public partial class MainForm : Form
     {
+        //create the necessary code for your program to run here
+        internal static Dictionary<string, Contact> Contacts = new Dictionary<string, Contact>();
+
         public MainForm()
         {
             InitializeComponent();
